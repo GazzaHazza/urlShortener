1d66153dba65e01bba8f556da3aae6b6aa91a248
Gary Hanks <gary.david.hanks@LEGO.com>
2017-07-24 19:44:27 +0100

    Stopped redux-logger in production

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {BrowserRouter} from 'react-router-dom';
-import {createStore, compose, applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
-import {createLogger} from 'redux-logger'
-import App from './App';
-import reducers from './reducers';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { createLogger } from "redux-logger";
+import App from "./App";
+import reducers from "./reducers";
+import registerServiceWorker from "./registerServiceWorker";
 
-let store = createStore(reducers, compose(applyMiddleware(thunk, createLogger()), window.devToolsExtension
-	? window.devToolsExtension()
-	: f => f))
+let middlewares = [thunk];
+
+if (process.env.NODE_ENV === `development`) {
+  middlewares.push(logger);
+}
+
+let store = createStore(
+  reducers,
+  compose(
+    applyMiddleware(...middlewares),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
 
 ReactDOM.render(
-	<Provider store={store}>
-	<BrowserRouter>
-		<App/>
-	</BrowserRouter>
-</Provider>, document.getElementById('root'));
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 
 registerServiceWorker();
