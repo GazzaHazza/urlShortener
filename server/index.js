c32ee071e99682395039cba5b4590486a5b141be
Gary Hanks <gary.david.hanks@LEGO.com>
2017-07-25 23:06:15 +0100

    moved server index.js in server folder

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 require('babel-register');
 
-const app = require('./server/app').app,
+const app = require('./app').app,
       PORT = process.env.PORT || 8000;
 app.listen(PORT, function() {
 	console.log('URL shortener microservice listening on port', PORT);
