18e62d2b28bfcbe60bb7a0b6dba2abbe1049480d
Gary Hanks <gary.david.hanks@LEGO.com>
2017-07-25 22:30:53 +0100

    fixed path again

--- server/app.js
+++ server/app.js
@@ -48,5 +48,5 @@ app.post('/api/new/', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+  res.sendFile(path.resolve(__dirname, '../client/build/index.html', 'index.html'));
 });
